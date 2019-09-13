4187f8a743686660ad92af7b66994e81aa9c275c
Erol Saroglu(BilgeAdam Ogrenci) <erol.saroglu@baogrenci.com>
2019-09-13 17:24:41 +0300

    Add project files.

---
+++ BilkentCafe/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BilkentCafe
+{
+    partial class frmAnasayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lswMasalar = new System.Windows.Forms.ListView();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.ürünlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geçmişSiparişlerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvwMasalar = new System.Windows.Forms.ListView();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 538);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lswMasalar
+            // 
+            this.lswMasalar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lswMasalar.Location = new System.Drawing.Point(0, 24);
+            this.lswMasalar.Name = "lswMasalar";
+            this.lswMasalar.Size = new System.Drawing.Size(800, 538);
+            this.lswMasalar.TabIndex = 1;
+            this.lswMasalar.UseCompatibleStateImageBehavior = false;
+            this.lswMasalar.SelectedIndexChanged += new System.EventHandler(this.lswMasalar_SelectedIndexChanged);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(4, 4, 0, 2);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ürünlerToolStripMenuItem,
+            this.geçmişSiparişlerToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // ürünlerToolStripMenuItem
+            // 
+            this.ürünlerToolStripMenuItem.Name = "ürünlerToolStripMenuItem";
+            this.ürünlerToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.ürünlerToolStripMenuItem.Text = "Ürünler";
+            // 
+            // geçmişSiparişlerToolStripMenuItem
+            // 
+            this.geçmişSiparişlerToolStripMenuItem.Name = "geçmişSiparişlerToolStripMenuItem";
+            this.geçmişSiparişlerToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.geçmişSiparişlerToolStripMenuItem.Text = "Geçmiş Siparişler";
+            // 
+            // lvwMasalar
+            // 
+            this.lvwMasalar.Location = new System.Drawing.Point(12, 37);
+            this.lvwMasalar.Name = "lvwMasalar";
+            this.lvwMasalar.Size = new System.Drawing.Size(776, 458);
+            this.lvwMasalar.TabIndex = 3;
+            this.lvwMasalar.UseCompatibleStateImageBehavior = false;
+            this.lvwMasalar.DoubleClick += new System.EventHandler(this.lvwMasalar_DoubleClick);
+            // 
+            // frmAnasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.lvwMasalar);
+            this.Controls.Add(this.lswMasalar);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAnasayfa";
+            this.Text = "Bilkent Cafe";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmAnasayfa_FormClosing);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListView lswMasalar;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem ürünlerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem geçmişSiparişlerToolStripMenuItem;
+        private System.Windows.Forms.ListView lvwMasalar;
+    }
+}
+
